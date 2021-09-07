e943457be416dc42823ee4a6642750cfc501f671
manbalboy <manbalboy@hanmail.net>
2021-09-08 00:38:55 +0900

    LinkedList 구현 - #7

---
+++ data-structure/src/test/java/linkedlist/SingleLinkedList.java
@@ -0,0 +1,82 @@
+package linkedlist;
+
+public class SingleLinkedList<T> {
+    Node<T> head = null;
+
+    public void addNode(T data) {
+        if (head == null) {
+            this.head = new Node<>(data);
+        } else {
+            Node<T> node = this.head;
+
+            while (node.next != null) {
+                node = node.next;
+            }
+
+            node.next = new Node<>(data);
+        }
+    }
+
+    public void printAll() {
+        if (head != null) {
+            Node<T> node = this.head;
+            while (node.next != null) {
+                node = node.next;
+                System.out.println(node.data);
+            }
+        }
+    }
+
+    public Node<T> searchNode(T data) {
+        if (this.head == null) {
+            return null;
+        } else {
+            Node<T> node = this.head;
+            while (node != null) {
+                if (node.data == data) {
+                    return node;
+                } else {
+                    node = node.next;
+                }
+            }
+            return null;
+        }
+    }
+
+    public void addNodeInside(T data, T isData) {
+        Node<T> searchedNode = this.searchNode(isData);
+
+        if (searchedNode == null) {
+            this.addNode(data);
+        } else {
+            Node<T> nextNode = searchedNode.next;
+            searchedNode.next = new Node<T>(data);
+            searchedNode.next.next = nextNode;
+        }
+    }
+
+
+    public boolean deleteNode(T isData) {
+        Node<T> searchedNode = this.searchNode(isData);
+
+        if (searchedNode == null) {
+            return false;
+        } else {
+            Node<T> node = this.head;
+            if(node.data == isData) {
+                this.head = this.head.next;
+                return true;
+            } else {
+                while (node.next != null) {
+                    if(node.next.data == isData) {
+                        node.next = node.next.next;
+                        return true;
+                    }
+                    node = node.next;
+                }
+                return false;
+            }
+        }
+
+    }
+}
