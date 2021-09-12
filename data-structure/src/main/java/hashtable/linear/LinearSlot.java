e6805ae3d2871bc7d9c3200e372d5e08a1b4ff71
manbalboy <manbalboy@hanmail.net>
2021-09-12 23:14:18 +0900

    linear hashtable 구현 - #12

---
+++ data-structure/src/main/java/hashtable/linear/LinearSlot.java
@@ -0,0 +1,11 @@
+package hashtable.linear;
+
+public class LinearSlot {
+    String key;
+    String value;
+
+    public LinearSlot(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+}
